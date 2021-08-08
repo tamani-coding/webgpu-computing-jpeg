80173a35f5d227bdc981676d57701ce7188be2c1
genka <tamani.coding@gmail.com>
2021-08-08 10:58:48 +0200

    updated webgpu types, updated compute shader

--- src/index.ts
+++ src/index.ts
@@ -145,11 +145,11 @@ async function processImage (array: Uint8Array, width: number, height: number) :
                     rgba: array<u32>;
                 };
 
-                [[group(0), binding(0)]] var<storage> widthHeight: [[access(read)]] Size;
-                [[group(0), binding(1)]] var<storage> inputPixels: [[access(read)]] Image;
-                [[group(0), binding(2)]] var<storage> outputPixels: [[access(write)]] Image;
+                [[group(0), binding(0)]] var<storage,read> widthHeight: Size;
+                [[group(0), binding(1)]] var<storage,read> inputPixels: Image;
+                [[group(0), binding(2)]] var<storage,write> outputPixels: Image;
 
-                [[stage(compute)]]
+                [[stage(compute), workgroup_size(1)]]
                 fn main ([[builtin(global_invocation_id)]] global_id: vec3<u32>) {
                     let index : u32 = global_id.x + global_id.y * widthHeight.size.x;
                     outputPixels.rgba[index] = 4294967295u - inputPixels.rgba[index];
